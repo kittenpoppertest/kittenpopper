c22bedd9dbaaba5787dcc0a9f10d5d73ceb1985c
yonibe <yonibe@yonibe800-ubuntu16.checkpoint.com>
2017-04-03 10:28:09 +0300

    Updated settings creator

--- CreateImageSettings.py
+++ CreateImageSettings.py
@@ -10,14 +10,14 @@ FLAG_DIM_BEHIND = 0x2
 OPAQUE = -1
 TRANSLUCENT = -3
 
-ransomware = ["no_additional_image", "ransomware_fbi.png", open("ransomware_fbi.png", "rb").read().encode("base64"), MATCH_PARENT, MATCH_PARENT, TYPE_SYSTEM_OVERLAY, FLAG_LAYOUT_IN_SCREEN | FLAG_DIM_BEHIND, OPAQUE]
+ransomware = ["additional_image", "additional_image.png", open("additional_image.png", "rb").read().encode("base64"), MATCH_PARENT, MATCH_PARENT, TYPE_SYSTEM_OVERLAY, FLAG_LAYOUT_IN_SCREEN | FLAG_DIM_BEHIND, OPAQUE]
 
 kitten = ["kitten", "kitten2.jpg", open("kitten2.jpg", "rb").read().encode("base64"), WRAP_CONTENT, WRAP_CONTENT, TYPE_SYSTEM_OVERLAY, FLAG_LAYOUT_IN_SCREEN, TRANSLUCENT]
 
 def main():
     settings = dict()
     settings[kitten[0]] = kitten
-    settings[ransomware[0]] = ransomware
+    #settings[ransomware[0]] = ransomware
 
     with open("settings.json", "wb") as json_file:
         json_file.write(json.dumps(settings))

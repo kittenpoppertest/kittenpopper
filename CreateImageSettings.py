36a1566d1f23b49eec63948c51e4c8c44cbf65e4
yonibe <yonibe@yonibe800-ubuntu16.checkpoint.com>
2017-04-03 10:26:06 +0300

    master Added settings creator and sample images

---
+++ CreateImageSettings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import json
+
+MATCH_PARENT = -1
+WRAP_CONTENT = -2
+TYPE_SYSTEM_OVERLAY = 2006
+FLAG_LAYOUT_IN_SCREEN = 0x100
+FLAG_DIM_BEHIND = 0x2
+OPAQUE = -1
+TRANSLUCENT = -3
+
+ransomware = ["no_additional_image", "ransomware_fbi.png", open("ransomware_fbi.png", "rb").read().encode("base64"), MATCH_PARENT, MATCH_PARENT, TYPE_SYSTEM_OVERLAY, FLAG_LAYOUT_IN_SCREEN | FLAG_DIM_BEHIND, OPAQUE]
+
+kitten = ["kitten", "kitten2.jpg", open("kitten2.jpg", "rb").read().encode("base64"), WRAP_CONTENT, WRAP_CONTENT, TYPE_SYSTEM_OVERLAY, FLAG_LAYOUT_IN_SCREEN, TRANSLUCENT]
+
+def main():
+    settings = dict()
+    settings[kitten[0]] = kitten
+    settings[ransomware[0]] = ransomware
+
+    with open("settings.json", "wb") as json_file:
+        json_file.write(json.dumps(settings))
+
+
+main()
